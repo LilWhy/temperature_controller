fceb6be53a58aeaceeb58a966a9fef675721c421
LilWhy <dremo9r0nd@gmail.com>
2021-05-10 01:27:43 +0400

    add test for rele and ternocoulpe

--- tests/rele_tests/turnOnOff.py
+++ tests/rele_tests/turnOnOff.py
@@ -1,1 +1,24 @@
-start
+import os
+import sys
+
+if not os.getegid() == 0:
+    sys.exit('Script must be run as root')
+    
+from time import sleep
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+
+signal = port.PG7
+
+gpio.init()
+gpio.setcfg(signal, gpio.OUTPUT)
+
+try:
+    print ("Press CTRL+C to exit")
+    while True:
+        gpio.output(signal, 1)
+        sleep(2)
+        gpio.output(signal, 0)
+     exept KeyboardInterrupt:
+        gpio.output(signal, 0)
+   
--- tests/termocouple_tests/temperature_take.py
+++ tests/termocouple_tests/temperature_take.py
@@ -1,1 +1,12 @@
-fd
+import spidev
+import time
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+while True:
+    resp = spi.readbytes(2)
+    #Умножаем на 0.25 для точности вычислений градусов
+    temp = ((resp[1] + resp[0]*256)/8)*0.25
+    print (temp)
+    time.sleep(1)
