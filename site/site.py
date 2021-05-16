ae8ea7065fa23e93b272c5254f71ab4f611266f5
LilWhy <dremo9r0nd@gmail.com>
2021-05-16 16:21:53 +0400

    routs

--- site/site.py
+++ site/site.py
@@ -4,26 +4,19 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/", methods=['GET'])
 def index():
-    print(request.method)
-    if request.method == 'POST':
-        if request.form.get('Start') == 'Start':
-           return render_template("index.html")
+     return render_template("index.html")
 
-        elif  request.form.get('Gaph') == 'Gaph':
-            return render_template("gaph.html")
+@app.route("/Start", methods=['GET'])
+def index():
+     return render_template("start.html")
 
-        elif  request.form.get('Modes') == 'Modes':
-            return render_template("modes.html")
-        else:
-                # pass # unknown
-            return render_template("index.html")
-    elif request.method == 'GET':
-            # return render_template("index.html")
-        print("No Post Back Call")
-    return render_template("index.html")
-
+@app.route("/Gaph", methods=['GET'])
+def index():
+     return render_template("gaph.html")
+     
 def cur_temp():
     return "hi"
 
