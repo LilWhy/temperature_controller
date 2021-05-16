9344374790e9e90679d44771ca4c1eed9dd16a10
LilWhy <dremo9r0nd@gmail.com>
2021-05-16 20:02:03 +0400

    add showing temperature

---
+++ site/temperature.py
@@ -0,0 +1,14 @@
+import spidev
+import time
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+resp = spi.readbytes(2)
+temp = ((resp[1] + resp[0]*256)/8)*0.25
+time.sleep(1)
+temp += ((resp[1] + resp[0]*256)/8)*0.25
+time.sleep(1)
+temp += ((resp[1] + resp[0]*256)/8)*0.25
+temp = temp // 3
+return temp
