84a18ce8ce8ceda7e6e4f4dad57fe7ca0a013ed3
LilWhy <dremo9r0nd@gmail.com>
2021-05-16 20:18:29 +0400

    add showing temperaturesas

--- site/site.py
+++ site/site.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, url_for, redirect, render_template
+import spidev
+import time
 
 
 app = Flask(__name__)
@@ -24,8 +26,20 @@ def graph():
         return redirect(url_for('graph'))
     return render_template("graph.html")
 
+@app.route("/graph", methods=['POST', 'GET']
 def cur_temp():
-    return "hi"
+    spi = spidev.SpiDev()
+    spi.open(0,0)
+
+    resp = spi.readbytes(2)
+    temp = ((resp[1] + resp[0]*256)/8)*0.25
+    time.sleep(1)
+    temp += ((resp[1] + resp[0]*256)/8)*0.25
+    time.sleep(1)
+    temp += ((resp[1] + resp[0]*256)/8)*0.25
+    temp = temp // 3
+    return (temp)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
