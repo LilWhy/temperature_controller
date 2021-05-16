df91c20efbcdaec774c5a0bdfd1b6347012ecf12
LilWhy <dremo9r0nd@gmail.com>
2021-05-16 13:51:22 +0400

    try to catch presses

--- site/site.py
+++ site/site.py
@@ -1,13 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+    from flask import Flask, render_template, request
 
 
+    app = Flask(__name__)
 
-@app.route('/index', methods=['GET', 'POST'])
-def mainpage():
-    return render_template("index.html")
-def hello():
-    return "Hi"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    @app.route("/", methods=['GET', 'POST'])
+    def index():
+        print(request.method)
+        if request.method == 'POST':
+            if request.form.get('Encrypt') == 'Encrypt':
+                # pass
+                print("Encrypted")
+            elif  request.form.get('Decrypt') == 'Decrypt':
+                # pass # do something else
+                print("Decrypted")
+            else:
+                # pass # unknown
+                return render_template("index.html")
+        elif request.method == 'GET':
+            # return render_template("index.html")
+            print("No Post Back Call")
+        return render_template("index.html")
+
+    if __name__ == '__main__':
+        app.run()
