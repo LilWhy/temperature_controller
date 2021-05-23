34763b01945117e71be0573c34a6968b732b468d
LilWhy <dremo9r0nd@gmail.com>
2021-05-24 02:59:15 +0400

    s

--- site/site.py
+++ site/site.py
@@ -32,7 +32,7 @@ def temperature():
     if temp <= 1:
         return ("Термопара не подключена")
     else:
-        return (temp))
+        return (temp)
 
 @app.route("/start_record", methods=['POST', 'GET'])
 def start_record():
